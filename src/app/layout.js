950083b9cd0cbc14be79928da2fabc211e60dafd
murodAli7 <murodalieshmamatov@gmai.com>
2026-01-27 17:18:57 +0500

    responsive error

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Overpass_Mono } from "next/font/google";
 import "./globals.css";
+import "./styles/styles.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
